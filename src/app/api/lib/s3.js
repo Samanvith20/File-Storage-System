50b06e6e8db9a31c443970802ceeb129cd62b1a6
Samanvith <Samanvith2005@gmail.com>
2025-07-27 10:36:50 +0530

    fix: Correct secret access key environment variable in S3 configuration because of reserved keyword in vercel

--- src/app/api/lib/s3.js
+++ src/app/api/lib/s3.js
@@ -1,8 +1,8 @@
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
   region: process.env.AWS_REGION,
 });
 // console.log('S3 Client Initialized:', s3.config);
