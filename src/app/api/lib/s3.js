ec34ada65ba08f7cdaac8f0f8e0b18de32452492
Samanvith <Samanvith2005@gmail.com>
2025-07-27 10:51:53 +0530

    fix: Update S3 bucket name and access key environment variables for consistency

--- src/app/api/lib/s3.js
+++ src/app/api/lib/s3.js
@@ -1,9 +1,9 @@
 import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.MY_ACCESS_KEY,
   secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-  region: process.env.AWS_REGION,
+  region: process.env.APP_REGION,
 });
 // console.log('S3 Client Initialized:', s3.config);
 
